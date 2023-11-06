251868bb3d8e5858961c1ff47dcc68939f47349f
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-11-06 21:14:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-import { companyOptions, companyInfo } from './data.js';
-
 const countrySelect = document.getElementById("countrySelect");
 const companySelect = document.getElementById("companySelect");
 const transactionSelect = document.getElementById("transactionSelect");
+const transactionToggle = document.getElementById("transactionToggle"); // Изменили идентификатор
+
 const greenBoxInfo = document.getElementById("greenBoxInfo");
 const yellowBoxCompanyInfo = document.getElementById("yellowBoxCompanyInfo");
 const redBoxCompanyInfo = document.getElementById("redBoxCompanyInfo");
 
 countrySelect.addEventListener("change", function () {
     const selectedCountry = this.value;
-    transactionSelect.disabled = false;
+    transactionToggle.disabled = false; // Изменили элемент, чтобы учитывать кнопки-переключатели
+    transactionSelect.disabled = false; // Добавили, чтобы учитывать выпадающий список
     if (selectedCountry === "default") {
+        transactionToggle.disabled = true;
         transactionSelect.disabled = true;
         companySelect.disabled = true;
-        transactionSelect.innerHTML = "<option value='default'>Select the type of transaction</option>";
-        companySelect.innerHTML = "<option value='default'>Select the type of payment</option>";
+        greenBoxInfo.textContent = "";
+        yellowBoxCompanyInfo.textContent = "";
+        redBoxCompanyInfo.textContent = "";
+    }
+});
+
+transactionToggle.addEventListener("click", function (event) { // Изменили обработчик событий
+    if (event.target.tagName === "BUTTON") {
+        const selectedCountry = countrySelect.value;
+        const selectedTransaction = event.target.getAttribute("data-value");
+        companySelect.disabled = false;
+        companySelect.innerHTML = "<option value='default'>Select the type of payment:</option>";
+
+        if (selectedTransaction !== "default" && selectedCountry in companyOptions) {
+            const companies = companyOptions[selectedCountry];
+            companies.forEach(function (company) {
+                const option = document.createElement("option");
+                option.value = company;
+                option.text = company;
+                companySelect.appendChild(option);
+            });
+        }
+
         greenBoxInfo.textContent = "";
         yellowBoxCompanyInfo.textContent = "";
         redBoxCompanyInfo.textContent = "";
@@ -24,25 +47,7 @@ countrySelect.addEventListener("change", function () {
 transactionSelect.addEventListener("change", function () {
     const selectedCountry = countrySelect.value;
     const selectedTransaction = this.value;
-    companySelect.disabled = false;
-    companySelect.innerHTML = "<option value='default'>Select the type of payment:</option>";
-
-    if (selectedTransaction !== "default" && selectedCountry in companyOptions) {
-        const companies = companyOptions[selectedCountry];
-        companies.forEach(function (company) {
-            const option = document.createElement("option");
-            option.value = company;
-            option.text = company;
-            companySelect.appendChild(option);
-        });
-    }
-});
-
-companySelect.addEventListener("change", function () {
-    const selectedCountry = countrySelect.value;
-    const selectedCompany = this.value;
-    const selectedTransaction = transactionSelect.value;
-    if (selectedTransaction !== "default" && selectedCompany !== "default") {
+    if (selectedCountry !== "default" && selectedTransaction !== "default") {
         const transactionInfo = companyInfo[selectedCountry][selectedTransaction][selectedCompany];
         greenBoxInfo.textContent = transactionInfo.greenBoxInfo;
         yellowBoxCompanyInfo.textContent = transactionInfo.yellowBoxInfo;

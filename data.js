b438174eb56f417c545bcb75932f6adc3aa545e5
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-11-07 12:23:20 +0200

    Update data.js

--- data.js
+++ data.js
@@ -47,7 +47,7 @@ const companyInfo = {
                 redBoxInfo: ""
             },
             Wirex: {
-                greenBoxInfo: ": I need to confirm, has easy bank transfer, seems to have bigger limits than Gemini, transferred in less than 12 hours to exodus.",
+                greenBoxInfo: "Need to confirm, has easy bank transfer, seems to have bigger limits than Gemini, transferred in less than 12 hours to exodus.",
                 yellowBoxInfo: "Checking a larger amount. 5,000 easy bank transfer instant.",
                 redBoxInfo: "Barclays and HSBC worked."
             },
@@ -74,7 +74,7 @@ Ireland: {
         },
         "Big deposit": {
             Wirex: {
-                greenBoxInfo: ": I need to confirm, has easy bank transfer, seems to have bigger limits than Gemini, transferred in less than 12 hours to exodus.",
+                greenBoxInfo: "Need to confirm, has easy bank transfer, seems to have bigger limits than Gemini, transferred in less than 12 hours to exodus.",
                 yellowBoxInfo: "Checking a larger amount. 5,000 easy bank transfer instant.",
                 redBoxInfo: "Barclays and HSBC worked."
             },

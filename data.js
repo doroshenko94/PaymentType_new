fc48737bdeba55ff8b558e13e6f0551b30cea651
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-11-08 20:34:06 +0200

    Update data.js

--- data.js
+++ data.js
@@ -23,16 +23,16 @@ const companyInfo = {
             },
             Curve: {
                 greenBoxInfo: "Curve you can download and install, credit the account and use virtual card on moonpay, similar process to revolut.",
-                yellowBoxInfo: "You can use credit card to top up which is useful",
-                redBoxInfo: "Not sure of age limit.",
+                yellowBoxInfo: "You can use credit card to top up which is useful. Not sure of age limit.",
+                redBoxInfo: "CURVE it is like revolut you can use a virtual card. You can top up with high street banks, you can use credit cards, and work with MOONPAY.",
             },
             Zen: {
-                greenBoxInfo: "Max you can do through Zen now is between 250$ - 10.000$ no more does not pass large amounts.",
+                greenBoxInfo: "Zen, you transfer max 5k here, you need to convert to ETH and leave for one day before you transfer the funds.",
                 yellowBoxInfo: "First transaction UK via wire must be 1.000$ only. Then you can do total 5.000$ next day",
                 redBoxInfo: "ZEN up to 2k: BANK TRANSFER: INSTANT VERIFICATIONs",
             },
             Wirex: {
-                greenBoxInfo: " -Name / -Email / -Address / -ID / -Selfie. Funding the account: - Easy bank transfer, put amount, select bank, prompts you to log into your online banking then you accept the transfer.         Changing from Fiat to crypto: - Only change to ETH OR BTC - Leave on the account for 24 hours  ",
+                greenBoxInfo: " -Name / -Email / -Address / -ID / -Selfie.  You have easy bank transfer, instantly lands, need to convert to ETH and leave for one day, withdrawals take 24-48 hours..         Changing from Fiat to crypto: - Only change to ETH OR BTC - Leave on the account for 24 hours  ",
                 yellowBoxInfo: "Withdrawing: - You can withdraw to an exodus wallet / - Withdrawals take 24-48 hours to approval",
                 redBoxInfo: "PLEASE NOTE THAT FOR BOTH GEMINI AND WIREX WITHDRAWALS ARE NOT INSTANT, WIREX SEEMS LIKE 24 HOURS MAX AND GEMINI CAN BE 5 BUSINESS DAYS. In these cases I would credit the money to the trading account so you are not held up, trade carefully and then wait until money is in the trading account before you make your next move, this is only to be done on the 3rd deposit of the client."
             },
@@ -74,17 +74,17 @@ const companyInfo = {
 Ireland: {
         STD: {
             Revolut: {
-                greenBoxInfo: "AGE: 18 - 69. If you have revolut more than 6 months you can use normally meaning that you can add 10k immediately if not then you are restricted to 2k per day and 10k per month",
-                yellowBoxInfo: "UK: First transfer through bank transfer max is 2k then you need to wait 24 hours and after you can do 10k per transaction",
-                redBoxInfo: "Revolut up to the age of 69 and then bintense: Open with them before you pitch, REVOLUT."
+                greenBoxInfo: "AGE: 18 - 69. - Revolut, you can not move more than 2k a day and 10k a month through crypto. You will need to convert the fiat to ETH/BTC and then transfer daily / 24 hours",
+                yellowBoxInfo: "You need to accept terms and conditions on conversion of crypto, this is a new step.",
+                redBoxInfo: "Revolut up to the age of 69 and then bintense: Open with them before you pitch, REVOLUT.",
             },
             Zen: {
-                greenBoxInfo: "Max you can do through Zen now is between 250$ - 10.000$ no more does not pass large amounts.",
+                greenBoxInfo: "Zen, you transfer max 5k here, you need to convert to ETH and leave for one day before you transfer the funds.",
                 yellowBoxInfo: "First transaction UK via wire must be 1.000$ only. Then you can do total 5.000$ next day",
-                redBoxInfo: "ZEN up to 2k: BANK TRANSFER: INSTANT VERIFICATIONs"
+                redBoxInfo: "ZEN up to 2k: BANK TRANSFER: INSTANT VERIFICATIONs",
             },
             Wirex: {
-                greenBoxInfo: " -Name / -Email / -Address / -ID / -Selfie. Funding the account: - Easy bank transfer, put amount, select bank, prompts you to log into your online banking then you accept the transfer.         Changing from Fiat to crypto: - Only change to ETH OR BTC - Leave on the account for 24 hours  ",
+                greenBoxInfo: " -Name / -Email / -Address / -ID / -Selfie.  You have easy bank transfer, instantly lands, need to convert to ETH and leave for one day, withdrawals take 24-48 hours..         Changing from Fiat to crypto: - Only change to ETH OR BTC - Leave on the account for 24 hours  ",
                 yellowBoxInfo: "Withdrawing: - You can withdraw to an exodus wallet / - Withdrawals take 24-48 hours to approval",
                 redBoxInfo: "PLEASE NOTE THAT FOR BOTH GEMINI AND WIREX WITHDRAWALS ARE NOT INSTANT, WIREX SEEMS LIKE 24 HOURS MAX AND GEMINI CAN BE 5 BUSINESS DAYS. In these cases I would credit the money to the trading account so you are not held up, trade carefully and then wait until money is in the trading account before you make your next move, this is only to be done on the 3rd deposit of the client."
             },
@@ -126,22 +126,22 @@ Ireland: {
     Italy: {
         STD: {
             Revolut: {
-                greenBoxInfo: "If you have Revolut more than 6 months you can use normally meaning that you can add 10k immediately if not then you are restricted to 2k per day and 10k per month",
-                yellowBoxInfo: "",
-                redBoxInfo: "Who Revolut works with: Bintense, Simplex, Zen"
+                greenBoxInfo: "AGE: 18 - 69. - Revolut, you can not move more than 2k a day and 10k a month through crypto. You will need to convert the fiat to ETH/BTC and then transfer daily / 24 hours",
+                yellowBoxInfo: "You need to accept terms and conditions on conversion of crypto, this is a new step.",
+                redBoxInfo: "Revolut up to the age of 69 and then bintense: Open with them before you pitch, REVOLUT.",
             },
             Bintence_Wire: {
                 greenBoxInfo: "",
                 yellowBoxInfo: "",
                 redBoxInfo: "You can not use BINTENSE WIRE"
             },
-            Zen: {
-                greenBoxInfo: "Max you can do through Zen now is between 250$ - 10.000$ no more does not pass large amounts.",
-                yellowBoxInfo: "Italy can do 5k first transaction one go",
-                redBoxInfo: ""
+           Zen: {
+                greenBoxInfo: "Zen, you transfer max 5k here, you need to convert to ETH and leave for one day before you transfer the funds.",
+                yellowBoxInfo: "First transaction UK via wire must be 1.000$ only. Then you can do total 5.000$ next day",
+                redBoxInfo: "ZEN up to 2k: BANK TRANSFER: INSTANT VERIFICATIONs",
             },
             Wirex: {
-                greenBoxInfo: " -Name / -Email / -Address / -ID / -Selfie. Funding the account: - Easy bank transfer, put amount, select bank, prompts you to log into your online banking then you accept the transfer.         Changing from Fiat to crypto: - Only change to ETH OR BTC - Leave on the account for 24 hours  ",
+                greenBoxInfo: " -Name / -Email / -Address / -ID / -Selfie.  You have easy bank transfer, instantly lands, need to convert to ETH and leave for one day, withdrawals take 24-48 hours..         Changing from Fiat to crypto: - Only change to ETH OR BTC - Leave on the account for 24 hours  ",
                 yellowBoxInfo: "Withdrawing: - You can withdraw to an exodus wallet / - Withdrawals take 24-48 hours to approval",
                 redBoxInfo: "PLEASE NOTE THAT FOR BOTH GEMINI AND WIREX WITHDRAWALS ARE NOT INSTANT, WIREX SEEMS LIKE 24 HOURS MAX AND GEMINI CAN BE 5 BUSINESS DAYS. In these cases I would credit the money to the trading account so you are not held up, trade carefully and then wait until money is in the trading account before you make your next move, this is only to be done on the 3rd deposit of the client."
             },
@@ -575,14 +575,14 @@ Ireland: {
     Switzerland: {
         STD: {
             Revolut: {
-                greenBoxInfo: "No information",
-                yellowBoxInfo: "",
-                redBoxInfo: "",
+                greenBoxInfo: "AGE: 18 - 69. - Revolut, you can not move more than 2k a day and 10k a month through crypto. You will need to convert the fiat to ETH/BTC and then transfer daily / 24 hours",
+                yellowBoxInfo: "You need to accept terms and conditions on conversion of crypto, this is a new step.",
+                redBoxInfo: "Revolut up to the age of 69 and then bintense: Open with them before you pitch, REVOLUT.",
             },
-            Zen: {
-                greenBoxInfo: "No information",
-                yellowBoxInfo: "",
-                redBoxInfo: "",
+             Zen: {
+                greenBoxInfo: "Zen, you transfer max 5k here, you need to convert to ETH and leave for one day before you transfer the funds.",
+                yellowBoxInfo: "First transaction UK via wire must be 1.000$ only. Then you can do total 5.000$ next day",
+                redBoxInfo: "ZEN up to 2k: BANK TRANSFER: INSTANT VERIFICATIONs",
             },
             Cryptocom: {
                 greenBoxInfo: "No information",

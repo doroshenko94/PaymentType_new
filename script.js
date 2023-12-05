134c8c8ead5b608952df9aae876406e295f56869
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-12-05 14:07:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ countrySelect.addEventListener("change", function () {
         greenBoxInfo.textContent = "";
         yellowBoxCompanyInfo.textContent = "";
         redBoxCompanyInfo.textContent = "";
-    } else if (companyInfo[selectedCountry]) { // Проверяем, есть ли информация о стране
+    } else if (companyInfo[selectedCountry]) {
         const transactionTypes = Object.keys(companyInfo[selectedCountry]);
         transactionTypes.forEach(function (type) {
             const button = document.createElement("button");
@@ -31,7 +31,6 @@ countrySelect.addEventListener("change", function () {
             transactionToggle.appendChild(button);
         });
     } else {
-        // Если информации о стране нет, вы можете отключить кнопки или выполнить другие действия по вашему усмотрению.
         transactionToggle.disabled = true;
         companySelect.disabled = true;
         greenBoxInfo.textContent = "Transaction types are not available for this country.";
@@ -39,7 +38,6 @@ countrySelect.addEventListener("change", function () {
         redBoxCompanyInfo.textContent = "";
     }
 });
-
 
 // Обработчик события выбора типа транзакции
 transactionToggle.addEventListener("click", function (event) {
@@ -73,69 +71,9 @@ transactionToggle.addEventListener("click", function (event) {
         yellowBoxCompanyInfo.textContent = "";
         redBoxCompanyInfo.textContent = "";
     }
-    
-    // Обработчик события выбора компании
-    companySelect.addEventListener("change", function () {
-        const selectedCountry = countrySelect.value;
-        const selectedTransaction = transactionToggle.querySelector(".btn.btn-outline-success.active").getAttribute("data-value");
-        const selectedCompany = this.value;
-
-     if (selectedCountry !== "default" && selectedTransaction !== "default" && selectedCompany !== "default") {
-    const transactionInfo = companyInfo[selectedCountry][selectedTransaction][selectedCompany];
-
-    if (transactionInfo) {
-        greenBoxInfo.textContent = transactionInfo.greenBoxInfo;
-        yellowBoxCompanyInfo.textContent = transactionInfo.yellowBoxInfo;
-        redBoxCompanyInfo.textContent = transactionInfo.redBoxInfo;
-
-        const imagesForCompany = imagesData[selectedCompany];
-
-        if (imagesForCompany) {
-            displayImages(imagesForCompany);
-        } else {
-            const imageContainer = document.getElementById("paymentImages");
-            imageContainer.innerHTML = "";
-        }
-    }
-} else {
-    greenBoxInfo.textContent = "";
-    yellowBoxCompanyInfo.textContent = "";
-    redBoxCompanyInfo.textContent = "";
-
-    const imageContainer = document.getElementById("paymentImages");
-    imageContainer.innerHTML = "";
-}
-    });
 });
 
-
-// Функция для отображения изображений в сетке
-function displayImages(images) {
-    const imageContainer = document.getElementById("paymentImages");
-    imageContainer.innerHTML = ""; // Очищаем контейнер перед добавлением новых изображений
-
-    images.forEach(function (imageUrl) {
-        const img = document.createElement("img");
-        img.src = imageUrl;
-        img.classList.add("payment-image");
-        img.addEventListener("click", function () {
-            const modal = document.getElementById("imageModal");
-            const modalImg = document.getElementById("modalImage");
-            modal.style.display = "block";
-            modalImg.src = this.src;
-        });
-        imageContainer.appendChild(img);
-    });
-}
-
-// Функция для закрытия модального окна
-document.querySelector(".close").addEventListener("click", function () {
-    const modal = document.getElementById("imageModal");
-    modal.style.display = "none";
-});
-
-
-// Изменение обработчика события для выбора компании платежа
+// Обработчик события выбора компании
 companySelect.addEventListener("change", function () {
     const selectedCountry = countrySelect.value;
     const selectedTransaction = transactionToggle.querySelector(".btn.btn-outline-success.active").getAttribute("data-value");
@@ -145,23 +83,11 @@ companySelect.addEventListener("change", function () {
         const transactionInfo = companyInfo[selectedCountry][selectedTransaction][selectedCompany];
 
         if (transactionInfo) {
-            // Если есть информация для выбранной компании, отображаем ее
             greenBoxInfo.textContent = transactionInfo.greenBoxInfo;
             yellowBoxCompanyInfo.textContent = transactionInfo.yellowBoxInfo;
             redBoxCompanyInfo.textContent = transactionInfo.redBoxInfo;
-        } else {
-            // Если информации нет, очищаем соответствующие блоки
-            greenBoxInfo.textContent = "";
-            yellowBoxCompanyInfo.textContent = "";
-            redBoxCompanyInfo.textContent = "";
-        }
-    } else {
-        greenBoxInfo.textContent = "";
-        yellowBoxCompanyInfo.textContent = "";
-        redBoxCompanyInfo.textContent = "";
-    }
 
-  const imagesForCompany = imagesData[selectedCompany];
+            const imagesForCompany = imagesData[selectedCompany];
 
             if (imagesForCompany) {
                 displayImages(imagesForCompany);
@@ -169,6 +95,13 @@ companySelect.addEventListener("change", function () {
                 const imageContainer = document.getElementById("paymentImages");
                 imageContainer.innerHTML = "";
             }
+        } else {
+            greenBoxInfo.textContent = "";
+            yellowBoxCompanyInfo.textContent = "";
+            redBoxCompanyInfo.textContent = "";
+
+            const imageContainer = document.getElementById("paymentImages");
+            imageContainer.innerHTML = "";
         }
     } else {
         greenBoxInfo.textContent = "";
@@ -194,12 +127,10 @@ async function fetchCurrencyRates() {
         const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd");
         const data = await response.json();
 
-        // Извлекаем курсы валют из полученных данных
         const btcPrice = data.bitcoin.usd;
         const ethPrice = data.ethereum.usd;
         const solPrice = data.solana.usd;
 
-        // Отображаем курсы валют на веб-странице
         document.getElementById("btcPrice").textContent = `BTC: $${btcPrice}`;
         document.getElementById("ethPrice").textContent = `ETH: $${ethPrice}`;
         document.getElementById("solPrice").textContent = `SOL: $${solPrice}`;
@@ -208,5 +139,27 @@ async function fetchCurrencyRates() {
     }
 }
 
-// Вызываем функцию для получения и отображения курсов валют
 fetchCurrencyRates();
+
+function displayImages(images) {
+    const imageContainer = document.getElementById("paymentImages");
+    imageContainer.innerHTML = "";
+
+    images.forEach(function (imageUrl) {
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        img.classList.add("payment-image");
+        img.addEventListener("click", function () {
+            const modal = document.getElementById("imageModal");
+            const modalImg = document.getElementById("modalImage");
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        });
+        imageContainer.appendChild(img);
+    });
+}
+
+document.querySelector(".close").addEventListener("click", function () {
+    const modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+});

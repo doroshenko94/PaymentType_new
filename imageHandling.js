9d08dc0a9731f060c4273faf22723a3241fc6d00
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-12-05 12:32:22 +0300

    Add files via upload

---
+++ imageHandling.js
@@ -0,0 +1,15 @@
+// Функция для обновления изображений для компании Coinjar
+function updateCoinjarImages() {
+    const imagesContainer = document.getElementById("greenBoxImages");
+    imagesContainer.innerHTML = ""; // Очистим контейнер перед добавлением новых изображений
+
+    // Допустим, у вас есть переменные с путями к изображениям для каждой компании:
+    const coinjarImages = ["1.jpg", "2.jpg", "3.jpg"];
+
+    coinjarImages.forEach((imagePath) => {
+        const img = document.createElement("img");
+        img.src = imagePath;
+        img.alt = "Coinjar Image";
+        imagesContainer.appendChild(img);
+    });
+}

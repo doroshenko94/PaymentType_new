40eedbc0d3cea466d3563221c9feed8d057c3816
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-12-14 13:53:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -176,6 +176,13 @@ const imagesData = {
         "Ndax_Images/Ndax6.JPG",
         // Добавьте пути к изображениям для каждой компании по аналогии
     ],
+       Wise: [
+        "Wise_Images/image.png",
+        "Wise_Images/image1.png",
+        "Wise_Images/image2.png",
+        "Wise_Images/image3.png",
+        // Добавьте пути к изображениям для каждой компании по аналогии
+    ],
     // Добавьте изображения для каждой компании по аналогии
 };
 

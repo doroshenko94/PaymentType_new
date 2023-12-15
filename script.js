1939517236412a6bf91efac94dacf0b0cccb38af
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-12-15 12:25:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,6 @@ const imagesData = {
         "Wise_Images/image3.png",
         // Добавьте пути к изображениям для каждой компании по аналогии
     ],
-    Bahama: {
     Banxa: [
         "Banxa_Images/image.png",
         "Banxa_Images/image1.png",
@@ -200,7 +199,6 @@ const imagesData = {
         // Добавьте пути к изображениям для каждой компании по аналогии
     ],
     // Добавьте изображения для каждой компании по аналогии
-    }
     };
 
 async function fetchCurrencyRates() {

9909f0346e631ea7e52f0ac2ef2daf442ca0644a
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2023-12-19 10:15:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -94,8 +94,13 @@ companySelect.addEventListener("change", function () {
             yellowBoxCompanyInfo.textContent = transactionInfo.yellowBoxInfo;
             redBoxCompanyInfo.textContent = transactionInfo.redBoxInfo;
 
-            const imagesForCompany = imagesData[selectedCompany];
-            const imagesForCompany = imagesData[selectedCountry] && imagesData[selectedCountry][selectedCompany];
+           let imagesForCompany;
+
+if (imagesData[selectedCountry] && imagesData[selectedCountry][selectedCompany]) {
+    imagesForCompany = imagesData[selectedCountry][selectedCompany];
+} else {
+    imagesForCompany = imagesData[selectedCompany];
+}
             const pdfsForCompany = pdfsData[selectedCompany]; // Добавлено для PDF файлов
             const textForCompany = textData[selectedCompany]; // Добавлено для текста
 

edddf2329b6879a0974f88b4934453d35762a24d
doroshenko94 <60771229+doroshenko94@users.noreply.github.com>
2024-12-11 17:08:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // script.js
-
-// Удаляем импорт!
-// import { companyOptions, companyInfo } from './data.js';
-
-const countrySelect = document.getElementById("countrySelect");
-const companySelect = document.getElementById("companySelect");
-const transactionToggle = document.getElementById("transactionToggle");
-
-const greenBoxInfo = document.getElementById("greenBoxInfo");
-const yellowBoxCompanyInfo = document.getElementById("yellowBoxCompanyInfo");
-const redBoxCompanyInfo = document.getElementById("redBoxCompanyInfo");
-
-// Определяем imagesData, pdfsData, textData и функции до их использования:
+// В этом файле хранятся функции отображения текста, изображений, PDF, а также данные imagesData, pdfsData, textData.
+// companyOptions и companyInfo хранятся в data.js (подключается до script.js).
 
 const countriesWithCommonWiseImages = ['Canada','New_Zeland','Bermuda','Oman','Saudi_Arabia','Greenland','Sweeden', 'South_Africa'];
 
@@ -24,36 +13,63 @@ const imagesData = {
             "Wise_Images/image3.png",
             "Wise_Images/image4.jpg",
             "Wise_Images/image5.jpg",
-            "Wise_Images/image6.jpg",
+            "Wise_Images/image6.jpg"
         ]
     },
     Coinjar: [
         "Coinjar_Images/coinjar1.jpg",
         "Coinjar_Images/coinjar2.jpg",
         "Coinjar_Images/coinjar3.jpg",
-        "Coinjar_Images/coinjar4.jpg",
+        "Coinjar_Images/coinjar4.jpg"
     ],
     First_direct_bank: [
         "First_direct_bank/1.jpg",
         "First_direct_bank/2.jpg",
-        // ... и так далее, все ваши изображения ...
-        "First_direct_bank/31.jpg",
+        "First_direct_bank/3.jpg",
+        "First_direct_bank/4.jpg",
+        "First_direct_bank/5.jpg",
+        "First_direct_bank/6.jpg",
+        "First_direct_bank/7.jpg",
+        "First_direct_bank/8.jpg",
+        "First_direct_bank/9.jpg",
+        "First_direct_bank/10.jpg",
+        "First_direct_bank/11.jpg",
+        "First_direct_bank/12.jpg",
+        "First_direct_bank/13.jpg",
+        "First_direct_bank/14.jpg",
+        "First_direct_bank/15.jpg",
+        "First_direct_bank/16.jpg",
+        "First_direct_bank/17.jpg",
+        "First_direct_bank/18.jpg",
+        "First_direct_bank/19.jpg",
+        "First_direct_bank/20.jpg",
+        "First_direct_bank/21.jpg",
+        "First_direct_bank/22.jpg",
+        "First_direct_bank/23.jpg",
+        "First_direct_bank/24.jpg",
+        "First_direct_bank/25.jpg",
+        "First_direct_bank/26.jpg",
+        "First_direct_bank/27.jpg",
+        "First_direct_bank/28.jpg",
+        "First_direct_bank/29.jpg",
+        "First_direct_bank/30.jpg",
+        "First_direct_bank/31.jpg"
     ],
     Newton: [
         "Newton_Images/1.png",
         "Newton_Images/2.png",
-        "Newton_Images/3.png",
+        "Newton_Images/3.png"
     ],
     GatePay: [
         "GatePay_Images/img.png",
-        "GatePay_Images/img1.png",
+        "GatePay_Images/img1.png"
     ],
     Safewirepay: [
         "Safewirepay_Images/1.png",
         "Safewirepay_Images/2.png",
         "Safewirepay_Images/3.png",
         "Safewirepay_Images/4.png",
-        "Safewirepay_Images/5.png",
+        "Safewirepay_Images/5.png"
     ],
     NDAX: [
         "Ndax_Images/Ndax1.JPG",
@@ -61,7 +77,7 @@ const imagesData = {
         "Ndax_Images/Ndax3.JPG",
         "Ndax_Images/Ndax4.JPG",
         "Ndax_Images/Ndax5.JPG",
-        "Ndax_Images/Ndax6.JPG",
+        "Ndax_Images/Ndax6.JPG"
     ],
     Banxa: [
         "Banxa_Images/image.png",
@@ -71,16 +87,16 @@ const imagesData = {
         "Banxa_Images/image4.png",
         "Banxa_Images/image5.png",
         "Banxa_Images/image6.png",
-        "Banxa_Images/image7.png",
+        "Banxa_Images/image7.png"
     ],
     Rampnetwork: [
-        "Rampnetwork_Images/image.png",
+        "Rampnetwork_Images/image.png"
     ],
     Easypayments : [
-        "Easypayments_Images/1.png",
+        "Easypayments_Images/1.png"
     ],
     Bybit : [
-        "ByBit_Images/image.png",
+        "ByBit_Images/image.png"
     ],
     Ziglu : [
         "Ziglu_Images/image.png",
@@ -91,10 +107,10 @@ const imagesData = {
         "Ziglu_Images/image (5).png",
         "Ziglu_Images/image (6).png",
         "Ziglu_Images/image (7).png",
-        "Ziglu_Images/image (8).png",
+        "Ziglu_Images/image (8).png"
     ],
     Coingira : [
-        "Coingira_Images/Coingira.png",
+        "Coingira_Images/Coingira.png"
     ],
     Xapo : [
         "Xapo/image.png",
@@ -107,13 +123,13 @@ const imagesData = {
         "Xapo/image (7).png",
         "Xapo/image (8).png",
         "Xapo/image (9).png",
-        "Xapo/image (10).png",
+        "Xapo/image (10).png"
     ],
     BitGet: [
         "BitGet_Images/1.png",
         "BitGet_Images/2.png",
         "BitGet_Images/3.png",
-        "BitGet_Images/4.png",
+        "BitGet_Images/4.png"
     ],
     Kraken: [
         "Kraken_Images/Kraken UK 1.png",
@@ -124,8 +140,8 @@ const imagesData = {
         "Kraken_Images/Kraken UK 6.png",
         "Kraken_Images/Kraken UK 7.png",
         "Kraken_Images/Kraken UK 8.png",
-        "Kraken_Images/Kraken UK 9.png",
-    ],
+        "Kraken_Images/Kraken UK 9.png"
+    ]
 };
 
 countriesWithCommonWiseImages.forEach(country => {
@@ -147,79 +163,77 @@ countriesWithCommonWiseImages.forEach(country => {
 
 const pdfsData = {
     Neteller: [
-        "PDFs/Countries in which it does not work.pdf",
+        "PDFs/Countries in which it does not work.pdf"
     ],
     Skrill: [
-        "PDFs/Countries in which it does not work Skrill.pdf",
+        "PDFs/Countries in which it does not work Skrill.pdf"
     ],
     Woltbit: [
-        "PDFs/Woltbit Guide.pdf",
+        "PDFs/Woltbit Guide.pdf"
     ],
     Safewirepay: [
         "PDFs/Private person KYC form.pdf",
         "PDFs/Private person KYC form - EXAMPLE.pdf",
-        "PDFs/Safewirepay step by step guide.pdf",
+        "PDFs/Safewirepay step by step guide.pdf"
     ],
     Revolut: [
-        "PDFs/Revolut.pdf",
+        "PDFs/Revolut.pdf"
     ],
     NDAX: [
-        "PDFs/Interac Deposit Steps.pdf",
+        "PDFs/Interac Deposit Steps.pdf"
     ],
     CoinPAPA: [
-        "PDFs/CoinPAPA questions for Video Call.pdf",
+        "PDFs/CoinPAPA questions for Video Call.pdf"
     ],
     Inserex: [
-        "PDFs/Insirex step-by-step guide.pdf",
+        "PDFs/Insirex step-by-step guide.pdf"
     ],
     GatePay: [
-        "PDFs/Standard KYC GatePay.pdf",
+        "PDFs/Standard KYC GatePay.pdf"
     ],
     TapBank: [
         "PDFs/Tap Questionaire.pdf",
-        "PDFs/Compliance SOW SOF Table.pdf",
+        "PDFs/Compliance SOW SOF Table.pdf"
     ],
     Paybis: [
-        "PDFs/Paybis.pdf",
+        "PDFs/Paybis.pdf"
     ],
     AnyCoinDirect: [
-        "PDFs/AnyCoinDirect Steps.pdf",
+        "PDFs/AnyCoinDirect Steps.pdf"
     ],
     Easypayments: [
-        "PDFs/Easypayments.ca.pdf",
+        "PDFs/Easypayments.ca.pdf"
     ],
     The_Voucher: [
-        "PDFs/TheVoucher.pdf",
+        "PDFs/TheVoucher.pdf"
     ],
     Sepa_Swift_Sortcode: [
-        "PDFs/SEPA_SWIFT_SORTCODE.pdf",
+        "PDFs/SEPA_SWIFT_SORTCODE.pdf"
     ],
     Coingira: [
         "PDFs/Coingira.pdf",
         "PDFs/Coingira Regular KYC List.pdf",
-        "PDFs/Coingira Exception KYC List.pdf",
+        "PDFs/Coingira Exception KYC List.pdf"
     ],
     Roobic: [
-        "PDFs/Roobic Q&A.pdf",
-    ],
+        "PDFs/Roobic Q&A.pdf"
+    ]
 };
 
 const textData = {
-    Simplex: "The only way to try these solutions - is to open WISE account personally...",
-    Adv2: "The only way to try these solutions...",
-    Advcash: "The only way to try these solutions...",
-    Bit2me: "The only way to try these solutions...",
-    Bestwallet: "The only way to try these solutions...",
-    Skrill: "The only way to try these solutions...",
-    Bybit: "The only way to try these solutions...",
-    Paybis: "The only way to try these solutions...",
-    MoonPay: "The only way to try these solutions...",
+    Simplex: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card on the payment solutions listed.",
+    Adv2: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    Advcash: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    Bit2me: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    Bestwallet: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    Skrill: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    Bybit: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    Paybis: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
+    MoonPay: "The only way to try these solutions - is to open WISE account personally, fund it in EUR and then use the virtual card.",
     Safewirepay: "<a href='https://safewirepay.com/'>Visit Safewirepay website</a>",
-    The_Voucher: "<a href='https://uiservices.vouchers-shops.com/...'>Visit TheVoucher website</a>",
-    // Добавьте другие тексты по аналогии
+    The_Voucher: "<a href='https://uiservices.vouchers-shops.com/(S(3gpp3a1iy3vg2stjkzltg5vn))/public/voucher/Product.aspx?merchantId=9412929&Item=7&Order=&Brand=J24001&AIID=10&VoucherType=&HPP=K+qscHXoDyaqDml5t+akLH1XlOpgjcoP0xHXMX9KSYM='>Visit TheVoucher website</a>"
 };
 
-// Функции displayText, displayImages, displayPDFLinks
 
 function displayText(text) {
     const textContainer = document.getElementById("textContainer");
@@ -268,156 +282,6 @@ function displayPDFLinks(pdfFiles) {
         link.textContent = fileName;
         link.classList.add("btn", "btn-outline-danger", "mr-2", "mb-2");
         link.setAttribute("target", "_blank");
-
         pdfContainer.appendChild(link);
     });
 }
-
-// Обработчики событий
-
-countrySelect.addEventListener("change", function () {
-    const selectedCountry = this.value;
-    transactionToggle.disabled = false;
-    companySelect.disabled = true;
-    transactionToggle.innerHTML = "";
-
-    if (selectedCountry === "default") {
-        transactionToggle.disabled = true;
-        companySelect.disabled = true;
-        greenBoxInfo.textContent = "";
-        yellowBoxCompanyInfo.textContent = "";
-        redBoxCompanyInfo.textContent = "";
-    } else if (companyInfo[selectedCountry]) {
-        const transactionTypes = Object.keys(companyInfo[selectedCountry]);
-        transactionTypes.forEach(function (type) {
-            const button = document.createElement("button");
-            button.type = "button";
-            button.className = "btn btn-outline-success";
-            button.setAttribute("data-value", type);
-            button.textContent = type;
-            transactionToggle.appendChild(button);
-        });
-    } else {
-        transactionToggle.disabled = true;
-        companySelect.disabled = true;
-        greenBoxInfo.textContent = "Transaction types are not available for this country.";
-        yellowBoxCompanyInfo.textContent = "";
-        redBoxCompanyInfo.textContent = "";
-    }
-});
-
-transactionToggle.addEventListener("click", function (event) {
-    if (event.target.tagName === "BUTTON") {
-        const buttons = this.querySelectorAll("button");
-        buttons.forEach(function (button) {
-            button.classList.remove("active");
-        });
-
-        event.target.classList.add("active");
-
-        const selectedCountry = countrySelect.value;
-        const selectedTransaction = event.target.getAttribute("data-value");
-        companySelect.disabled = false;
-        companySelect.innerHTML = "<option value='default'>Select the type of payment:</option>";
-
-        if (selectedTransaction !== "default" && selectedCountry in companyOptions) {
-            const companies = companyOptions[selectedCountry];
-
-            companies.forEach(function (company) {
-                if (companyInfo[selectedCountry][selectedTransaction][company]) {
-                    const option = document.createElement("option");
-                    option.value = company;
-                    option.text = company;
-                    companySelect.appendChild(option);
-                }
-            });
-        }
-
-        greenBoxInfo.textContent = "";
-        yellowBoxCompanyInfo.textContent = "";
-        redBoxCompanyInfo.textContent = "";
-    }
-});
-
-companySelect.addEventListener("change", function () {
-    const selectedCountry = countrySelect.value;
-    const activeButton = transactionToggle.querySelector(".btn.btn-outline-success.active");
-    if(!activeButton) return;
-
-    const selectedTransaction = activeButton.getAttribute("data-value");
-    const selectedCompany = this.value;
-
-    if (selectedCountry !== "default" && selectedTransaction !== "default" && selectedCompany !== "default") {
-        const transactionInfo = companyInfo[selectedCountry][selectedTransaction][selectedCompany];
-
-        if (transactionInfo) {
-            greenBoxInfo.textContent = transactionInfo.greenBoxInfo || "";
-            yellowBoxCompanyInfo.textContent = transactionInfo.yellowBoxInfo || "";
-            redBoxCompanyInfo.textContent = transactionInfo.redBoxInfo || "";
-
-            let imagesForCompany;
-            if (imagesData[selectedCountry] && imagesData[selectedCountry][selectedCompany]) {
-                imagesForCompany = imagesData[selectedCountry][selectedCompany];
-            } else {
-                imagesForCompany = imagesData[selectedCompany];
-            }
-
-            const pdfsForCompany = pdfsData[selectedCompany];
-            const textForCompany = textData[selectedCompany];
-
-            if (textForCompany) {
-                displayText(textForCompany);
-                const textContainer = document.getElementById("textContainer");
-                textContainer.classList.remove("d-none");
-                textContainer.classList.add("mt-3", "p-3", "bg-light");
-                const paragraphElement = textContainer.querySelector("p");
-                if (paragraphElement) {
-                    paragraphElement.classList.add("text-warning");
-                }
-            } else {
-                const textContainer = document.getElementById("textContainer");
-                textContainer.textContent = "";
-                textContainer.className = "d-none";
-            }
-
-            if (imagesForCompany) {
-                displayImages(imagesForCompany);
-            } else {
-                const imageContainer = document.getElementById("paymentImages");
-                imageContainer.innerHTML = "";
-            }
-
-            const pdfContainer = document.getElementById("pdfFiles");
-            pdfContainer.innerHTML = "";
-
-            if (pdfsForCompany) {
-                displayPDFLinks(pdfsForCompany);
-            }
-        } else {
-            greenBoxInfo.textContent = "";
-            yellowBoxCompanyInfo.textContent = "";
-            redBoxCompanyInfo.textContent = "";
-
-            const imageContainer = document.getElementById("paymentImages");
-            imageContainer.innerHTML = "";
-
-            const pdfContainer = document.getElementById("pdfFiles");
-            pdfContainer.innerHTML = "";
-        }
-    } else {
-        greenBoxInfo.textContent = "";
-        yellowBoxCompanyInfo.textContent = "";
-        redBoxCompanyInfo.textContent = "";
-
-        const imageContainer = document.getElementById("paymentImages");
-        imageContainer.innerHTML = "";
-
-        const pdfContainer = document.getElementById("pdfFiles");
-        pdfContainer.innerHTML = "";
-    }
-});
-
-document.querySelector(".close").addEventListener("click", function () {
-    const modal = document.getElementById("imageModal");
-    modal.style.display = "none";
-});
